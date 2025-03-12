1665fbbaef73de641f0a2bd971753b7897b0b827
David Ambrozio <davidambroziodev@gmail.com>
2025-03-12 10:39:32 -0300

    feat: create main.py

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+service = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service)
+
+driver.get("https://www.google.com")
