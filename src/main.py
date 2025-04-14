10155914d9a20dfd239f00040753ef50165694f1
David Ambrozio <davidambroziodev@gmail.com>
2025-04-14 19:22:42 -0300

    feat: update main.py

--- src/main.py
+++ src/main.py
@@ -1,4 +1,8 @@
-from handlers.instagram_handler import send_instagram_message
+from handlers.instagram_handler import send_messages_to_users
+from handlers.file_handler import read_usernames_from_file
 
 if __name__ == "__main__":
-    send_instagram_message()
+    filepath = "src/data/profiles.txt"
+    usernames = read_usernames_from_file(filepath)
+    mensagem = "Olá! Essa é uma mensagem automática personalizada para mais de um perfil."
+    send_messages_to_users(usernames, mensagem)
