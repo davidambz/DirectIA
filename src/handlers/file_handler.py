62b174aad20200fad61acbc3deafb5e2e3334d15
David Ambrozio <davidambroziodev@gmail.com>
2025-06-30 21:58:17 -0300

    fix: adjust file path resolution for profiles.txt to support both dev and executable modes

--- src/handlers/file_handler.py
+++ src/handlers/file_handler.py
@@ -1,29 +1,26 @@
 import csv
 import os
+import sys
 
 def get_base_path():
-    # Vai até a raiz do projeto, subindo 2 níveis
-    return os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
+    if getattr(sys, 'frozen', False):
+        return os.path.dirname(sys.executable)
+    else:
+        return os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "data"))
 
 def read_usernames_from_file(filename: str) -> list[str]:
     base_path = get_base_path()
-
     filepath = os.path.join(base_path, filename)
-    dev_path = os.path.join(base_path, "src", "data", filename)
 
-    if os.path.exists(filepath):
-        final_path = filepath
-    elif os.path.exists(dev_path):
-        final_path = dev_path
-    else:
-        raise FileNotFoundError(f"Arquivo não encontrado: {filepath} ou {dev_path}")
+    if not os.path.exists(filepath):
+        raise FileNotFoundError(f"Arquivo não encontrado: {filepath}")
 
-    _, ext = os.path.splitext(final_path)
+    _, ext = os.path.splitext(filepath)
 
     if ext.lower() == ".txt":
-        return _read_from_txt(final_path)
+        return _read_from_txt(filepath)
     elif ext.lower() == ".csv":
-        return _read_from_csv(final_path)
+        return _read_from_csv(filepath)
     else:
         raise ValueError("Formato de arquivo não suportado. Use .txt ou .csv.")
 
